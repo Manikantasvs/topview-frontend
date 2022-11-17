c204c48e8e5b2a6c82f777581d5ea47c64245599
manikantas <manikantas@turito.com>
2022-11-17 16:55:48 +0530

    added ng-bootstrap and few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingHeaderComponent } from './landing-header/landing-header.component';
+import { LandingBodyComponent } from './landing-body/landing-body.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LandingHeaderComponent,
+    LandingBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
